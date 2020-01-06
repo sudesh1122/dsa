c5e786adde79927393d4159e9951b05e99200d97
Sudesh1122 <sudesh18@iitg.ac.in>
2020-01-06 13:46:29 +0530

    Delete function added

--- bst.cpp
+++ bst.cpp
@@ -43,6 +43,65 @@ bool Search(Node* root, int data)
 	else return Search(root->r,data);
 }
 
+
+
+Node* FindMin(Node* n)
+{
+	Node * temp = n;
+	while(temp->l!=NULL)
+	{
+		temp = temp->l;
+	}
+	return temp;
+
+}
+
+
+
+Node* Delete(Node* root, int data)
+{
+	if(root==NULL)
+	{
+		return root;
+	}
+	else if(data < root->data) root->l = Delete(root->l,data);
+	else if(data > root->data) root->r = Delete(root->r,data);
+	else // when the node found
+	{	
+		//case 1 when node to be deleted has no child
+		if(root->l ==NULL && root->r==NULL)
+		{
+			delete root;
+			root = NULL;
+			
+		}
+		//case 2. when node to be deleted has 1 chid 
+		else if(root->l == NULL)
+		{
+			Node* temp = root;
+			root = root->r;
+			delete temp;
+			
+		}
+		else if(root->r == NULL)
+		{
+			Node* temp = root;
+			root = root->l;
+			delete temp;
+			
+		}
+		//case3. when node to be deleted has 2 children
+		else
+		{
+			Node* temp = FindMin(root->r); //or FindMax(root->l);
+			root->data = temp->data;
+			root->r = Delete(root->r,temp->data);
+		} 
+	}
+	return root;	
+
+}
+
 int main()
 {
 	Node* root = NULL;
@@ -52,9 +111,17 @@ int main()
 	root = Insert(root,25);
 	root = Insert(root,8);
 	root = Insert(root,12);
+
 	int num;
 	cout<<"Enter the number you want to Search- \n";
 	cin>>num;
 	if(Search(root,num)) cout<<"FOUND!!!\n";
 	else cout<<"Not FOUND!!\n";
+	root = Delete(root,10);
+	
+	cout<<"Enter the number you want to Search- \n";
+	cin>>num;
+	if(Search(root,num)) cout<<"FOUND!!!\n";
+	else cout<<"Not FOUND!!\n";
+
 }
